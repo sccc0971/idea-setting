39121db6b2d322f301948d1d7cc6837ddc30bffa
sccc0971 <672537273@qq.com>
2018-12-01 18:04:13 +0800

    IU-2018.2.5 <Administrator@SC-201809040002 Create ide.general.xml, git.xml, project.default.xml, laf.xml, other.xml, Class.java, jdk.table.xml, debugger.renderers.xml, Default.xml, Custom.xml, path.macros.xml, file.template.settings.xml, projectView.xml, vcs.xml, editor.codeinsight.xml, colors.scheme.xml, github_settings.xml, templates.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+  @Description description
+  @Author ${USER}
+  @Date ${DATE} ${TIME}
+ **/
+public class ${NAME} {
+}
